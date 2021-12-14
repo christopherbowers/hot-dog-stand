0f3516838a271a7a3797968c18f837d117184e38
BenjaminoBambino <benjaminlpeck@gmail.com>
2021-12-14 08:18:57 -0800

    move props to App.js

--- client/src/gameplay/CheckInventory.jsx
+++ client/src/gameplay/CheckInventory.jsx
@@ -1,35 +1,9 @@
-import React, { useEffect, useState } from "react";
-import axios from 'axios';
+import React from "react";
 import MainCard from "../components/MainCard";
 import SideCard from "../components/SideCard";
 import DrinkCard from "../components/DrinkCard";
 
-const CheckInventory = () => {
-
-  const[mains, setMains] = useState([]);
-  const[sides, setSides] = useState([]);
-  const[drinks, setDrinks] = useState([]);
-
-  const getMains = async () => {
-    const res = await axios.get('http://localhost:3001/api/mains');
-    setMains(res.data.mains);
-  }
-
-  const getSides = async () => {
-    const res = await axios.get('http://localhost:3001/api/sides');
-    setSides(res.data.sides);
-  }
-
-  const getDrinks = async () => {
-    const res = await axios.get('http://localhost:3001/api/drinks');
-    setDrinks(res.data.drinks);
-  }
-
-  useEffect(() => {
-    getMains();
-    getSides();
-    getDrinks();
-  }, []);
+const CheckInventory = (props) => {
 
   return (
     <div>
@@ -37,34 +11,36 @@ const CheckInventory = () => {
       <section className="inventory-container">
         <section className="inventory-items">
           <h3>Mains</h3>
-          {mains.map((main) => (
-            <MainCard 
-              key={main._id}
-              name={main.name}
-              quantity={main.quantity}
-              price={main.price}
-            />
-          ))}
+          {
+            props.mains.map((main) => (
+              <MainCard 
+                key={main._id}
+                name={main.name}
+                quantity={main.quantity}
+                price={main.price}
+              />
+            ))
+          }
         </section>
         <section className="inventory-items">
           <h3>Sides</h3>
-          {sides.map((side) => (
+          {props.sides.map((side) => (
             <SideCard 
-              key={side._id}
-              name={side.name}
-              quantity={side.quantity}
-              price={side.price}
+              key={ side._id }
+              name={ side.name }
+              quantity={ side.quantity }
+              price={ side.price }
             />
           ))}
         </section>
         <section className="inventory-items">
           <h3>Drinks</h3>
-          {drinks.map((drink) => (
+          {props.drinks.map((drink) => (
             <DrinkCard 
-              key={drink._id}
-              name={drink.name}
-              quantity={drink.quantity}
-              price={drink.price}
+              key={ drink._id }
+              name={ drink.name }
+              quantity={ drink.quantity }
+              price={ drink.price }
             />
           ))}
         </section>
