1983350ebbf4d7bda95cf4a43ae82f35eb235b70
BenjaminoBambino <benjaminlpeck@gmail.com>
2021-12-14 15:58:29 -0800

    add instructions component and paths to reach it

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
   return(
     <div className="sid-wall">
       <p>Welcome to the Eats-A-Lot Hot Dog Stand! The Rides-A-Lot Theme Park is open and your concession stand is right at the center of the action. Do you have what it takes to feed the multitudes?</p>
+      <Link to="/instructions">Review the Instructions</Link>
       <Link to="/game">Start Cooking</Link>
     </div>
   )
