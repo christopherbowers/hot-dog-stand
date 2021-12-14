348dbfdc18c1225b1478b1feb8a4635bf92ff1c9
BenjaminoBambino <benjaminlpeck@gmail.com>
2021-12-14 11:33:40 -0800

    displying partial game logic

--- client/src/gameplay/OpenShop.jsx
+++ client/src/gameplay/OpenShop.jsx
@@ -1,19 +1,96 @@
-import React from "react";
+import React from "react"
+import GameNav from "../components/GameNav"
 
 const OpenShop = (props) => {
-  console.log(props.mains)
-  // const burgerCount = parseInt(props.mains[0].quantity)
-  // const burgersSold = Math.floor(Math.random * burgerCount);
-  // const burgersLeft = burgerCount - burgersSold;
+  console.log(props)
 
-  // console.log(burgersLeft);
+  // Mains
+  const burgers = props.mains[0]
+  const initialBurgerCount = parseInt(burgers.quantity)
+  const burgersSold = Math.floor(Math.random() * initialBurgerCount);
+  const burgersLeft = initialBurgerCount - burgersSold;
+
+  const hotDogs = props.mains[1]
+  const initialHotDogCount = parseInt(hotDogs.quantity)
+  const hotDogsSold = Math.floor(Math.random() * initialHotDogCount);
+  const hotDogsLeft = initialHotDogCount - hotDogsSold;
+
+  const wings = props.mains[2]
+  const initialWingsCount = parseInt(wings.quantity)
+  const wingsSold = Math.floor(Math.random() * initialWingsCount);
+  const wingsLeft = initialWingsCount - wingsSold;
+
+  // Sides
+
+  // Drinks
 
   return (
     <div>
+      <GameNav />
       <h2>Open Shop</h2>
+      <section className="mains-container">
+        <h3>Mains</h3>
+        <div className="results-item">
+          <h4>{ burgers.name }</h4>
+          <h5>Starting Count:</h5>
+          <p>{ initialBurgerCount }</p>
+          <h5>Items Sold:</h5>
+          <p>{ burgersSold }</p>
+          <h5>Final Count:</h5>
+          <p>{ burgersLeft }</p>
+        </div>
+        <div className="results-item">
+          <h4>{ hotDogs.name }</h4>
+          <h5>Starting Count:</h5>
+          <p>{ initialHotDogCount }</p>
+          <h5>Items Sold:</h5>
+          <p>{ hotDogsSold }</p>
+          <h5>Final Count:</h5>
+          <p>{ hotDogsLeft }</p>
+        </div>
+        <div className="results-item">
+          <h4>{ wings.name }</h4>
+          <h5>Starting Count:</h5>
+          <p>{ initialWingsCount }</p>
+          <h5>Items Sold:</h5>
+          <p>{ wingsSold }</p>
+          <h5>Final Count:</h5>
+          <p>{ wingsLeft }</p>
+        </div>
+      </section>
+      <section className="sides-container">
+        <h3>Sides</h3>
+        <div className="results-item">
+          <h4>{ burgers.name }</h4>
+          <h5>Starting Count:</h5>
+          <p>{ initialBurgerCount }</p>
+          <h5>Items Sold:</h5>
+          <p>{ burgersSold }</p>
+          <h5>Final Count:</h5>
+          <p>{ burgersLeft }</p>
+        </div>
+        <div className="results-item">
+          <h4>{ hotDogs.name }</h4>
+          <h5>Starting Count:</h5>
+          <p>{ initialHotDogCount }</p>
+          <h5>Items Sold:</h5>
+          <p>{ hotDogsSold }</p>
+          <h5>Final Count:</h5>
+          <p>{ hotDogsLeft }</p>
+        </div>
+        <div className="results-item">
+          <h4>{ wings.name }</h4>
+          <h5>Starting Count:</h5>
+          <p>{ initialWingsCount }</p>
+          <h5>Items Sold:</h5>
+          <p>{ wingsSold }</p>
+          <h5>Final Count:</h5>
+          <p>{ wingsLeft }</p>
+        </div>
+      </section>
 
     </div>
   )
 }
 
-export default OpenShop;
+export default OpenShop
