ce146e901dbaca81dc61a7cefa265df6cb3831bc
BenjaminoBambino <benjaminlpeck@gmail.com>
2021-12-13 17:14:49 -0800

    add browser router

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Nav from './components/Nav';
+import Game from './pages/Game';
+import Instructions from './pages/Instructions';
+import About from './pages/About';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <main>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/game" component={Game} />
+          <Route path="/instructions" component={Instructions} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
