69aa1fcea850cd630b6facc3c99daeb812b27af9
Christopher Bowers <c_bowers@me.com>
2021-12-13 22:09:45 -0500

    Set up GET routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,9 @@ const Main = require('../models/main')
 const Side = require('../models/side')
 const Drink = require('../models/drink')
 
+/* GET 
+--------------------------------------------*/
+
 const getAllMains = async (req, res) => {
   try {
     const mains = await Main.find()
@@ -32,8 +35,79 @@ const getAllDrinks = async (req, res) => {
   }
 }
 
+/* PUT 
+--------------------------------------------*/
+
+//TODO: convert params to lowercase
+
+// app.get('/fruits/:name', (req, res) => {
+//   let lowerCaseFruit = (req.params.name).toLowerCase()
+//   const fruit = fruits.find( ({ name }) => name.toLowerCase() === lowerCaseFruit )
+//   res.send(fruit.name)
+// })
+
+
+const updateMains = async (req, res) => {
+  try {
+    const { name } = req.params;
+    await Main.findOneAndUpdate(name, req.body, (err, main) => {
+      if (err) {
+        res.status(500).send(err);
+      }
+      if (!main) {
+        res.status(500).send('Main not found!');
+      }
+      return res.status(200).json(main);
+    }).clone().catch()
+  } 
+  catch (error) {
+    return res.status(501).send(error.message)
+  }
+}
+
+const updateSides = async (req, res) => {
+  try {
+    const { name } = req.params;
+    await Side.findOneAndUpdate(name, req.body, (err, side) => {
+      if (err) {
+        res.status(500).send(err);
+      }
+      if (!main) {
+        res.status(500).send('Side not found!');
+      }
+      return res.status(200).json(side);
+    }).clone().catch()
+  } 
+  catch (error) {
+    return res.status(501).send(error.message)
+  }
+}
+
+const updateDrinks = async (req, res) => {
+  try {
+    const { name } = req.params;
+    await Drink.findOneAndUpdate(name, req.body, (err, drink) => {
+      if (err) {
+        res.status(500).send(err);
+      }
+      if (!main) {
+        res.status(500).send('Drink not found!');
+      }
+      return res.status(200).json(drink);
+    }).clone().catch()
+  } 
+  catch (error) {
+    return res.status(501).send(error.message)
+  }
+}
+
+
+
 module.exports = {
   getAllMains,
   getAllSides,
-  getAllDrinks  
+  getAllDrinks,
+  updateMains,
+  updateSides,
+  updateDrinks
 }
