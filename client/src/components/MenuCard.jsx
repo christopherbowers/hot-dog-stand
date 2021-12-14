3759bd32f743e6d9db222cf92b690fad0be7684e
Sid Meka <gsmekarrow@gmail.com>
2021-12-14 15:17:22 -0500

    Nice Hovering Effects

--- client/src/components/MenuCard.jsx
+++ client/src/components/MenuCard.jsx
@@ -1,11 +1,16 @@
+import { NavLink } from "react-router-dom"
+
 const MenuCard = (props) => {
 
   return (
+    <NavLink to="/game/set-inventory">
     <div className="menu-card">
-        <h4>{props.name}</h4>
-        <p>Quantity: {props.quantity}</p>
-        <p class="price">Price: ${props.price}</p>
-    </div>
+      
+      <h4>{props.name}</h4>
+      <p>Quantity: {props.quantity}</p>
+      <p class="price">Price: ${props.price}</p>
+      
+    </div></NavLink>
   )
 }
 
