80ed647e94adae1137e37f956c72adf3ff3ed1ef
Christopher Bowers <c_bowers@me.com>
2021-12-14 11:33:08 -0500

    Change card to a single component

--- client/src/components/MenuCard.jsx
+++ client/src/components/MenuCard.jsx
@@ -1,14 +1,12 @@
-import React from 'react';
-
-const DrinkCard = (props) => {
+const MenuCard = (props) => {
 
   return (
     <div className="menu-card">
-       <h4>{props.name}</h4>
+        <h4>{props.name}</h4>
         <p>Quantity: {props.quantity}</p>
         <p>Price: ${props.price}</p>
     </div>
   )
 }
 
-export default DrinkCard
+export default MenuCard
