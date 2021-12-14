53da42d69ecb04dd668bdc8f3729fbb2559dacc1
BenjaminoBambino <benjaminlpeck@gmail.com>
2021-12-14 12:08:00 -0800

    updated OpenShop

--- client/src/gameplay/OpenShop.jsx
+++ client/src/gameplay/OpenShop.jsx
@@ -21,6 +21,20 @@ const OpenShop = (props) => {
   const wingsLeft = initialWingsCount - wingsSold;
 
   // Sides
+  const fries = props.sides[0]
+  const initialFriesCount = parseInt(fries.quantity)
+  const friesSold = Math.floor(Math.random() * initialFriesCount);
+  const friesLeft = initialFriesCount - friesSold;
+
+  // const hotDogs = props.mains[1]
+  // const initialHotDogCount = parseInt(hotDogs.quantity)
+  // const hotDogsSold = Math.floor(Math.random() * initialHotDogCount);
+  // const hotDogsLeft = initialHotDogCount - hotDogsSold;
+
+  // const wings = props.mains[2]
+  // const initialWingsCount = parseInt(wings.quantity)
+  // const wingsSold = Math.floor(Math.random() * initialWingsCount);
+  // const wingsLeft = initialWingsCount - wingsSold;
 
   // Drinks
 
@@ -61,13 +75,13 @@ const OpenShop = (props) => {
       <section className="sides-container">
         <h3>Sides</h3>
         <div className="results-item">
-          <h4>{ burgers.name }</h4>
+          <h4>{ fries.name }</h4>
           <h5>Starting Count:</h5>
-          <p>{ initialBurgerCount }</p>
+          <p>{ initialFriesCount }</p>
           <h5>Items Sold:</h5>
-          <p>{ burgersSold }</p>
+          <p>{ friesSold }</p>
           <h5>Final Count:</h5>
-          <p>{ burgersLeft }</p>
+          <p>{ friesLeft }</p>
         </div>
         <div className="results-item">
           <h4>{ hotDogs.name }</h4>
